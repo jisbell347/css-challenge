0e70707159b8fb35140ae9cd04db38efb83d09c3
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-25 22:36:03 -0600

    Added classes and properties for about-me section

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
 			<!-- HEADER -->
 			<header>
 				<div class="container header">
-					<h1>Dylan McDonald:~$</h1>
-					<em class="header-description">Computer Scientist. Cat Lover. Picard Fan.</em>
+					<h1 class="header-name">Dylan McDonald:~$</h1>
+					<em>Computer Scientist. Cat Lover. Picard Fan.</em>
 				</div>
 			</header>
 
@@ -45,11 +45,11 @@
 
 			<!-- ABOUT ME -->
 			<section class="about-me">
-				<div class="container">
-					<img src="images/stunning.jpg" alt="captain picard" />
-					<h2>A Day in the Life...</h2>
-					<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
-					<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
+				<div class="container about-me-wrapper">
+					<img src="images/stunning.jpg" alt="captain picard" class="picard-image"/>
+					<h2 class="about-me-header">A Day in the Life...</h2>
+					<p class="about-me-text">Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
+					<p class="about-me-text">Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
 					<div class="clearfix"></div>
 				</div>
 			</section>
